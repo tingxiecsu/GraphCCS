e9d416b05a5a1ea7ddcd34e8b62877fcf92690f4
tingxiecsu <81673071+tingxiecsu@users.noreply.github.com>
2022-06-10 11:34:35 +0800

    Add files via upload

---
+++ GraphCCS/run_preidct.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  9 15:44:15 2022
+
+@author: ZNDX002
+"""
+
+from train import Predict
+import yaml
+import pandas as pd
+
+
+def main():
+    config = yaml.load(open("/config/config.yaml", "r"), Loader=yaml.FullLoader)
+    dataset = pd.read_csv('/data/')
+    model_path = 'C:/project/model/model.pt'
+    model_predict = Predict(dataset,model_path,**config)
+    y=model_predict.ccs_predict()
+    dataset['predicts']=''
+    for i in range(len(dataset['SMILES'])):
+        dataset['predicts'][i]=y[i]
+
+if __name__ == "__main__":
+    main()
